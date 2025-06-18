d0a5443cbef4a6905fdd7fb7421cd73cf3a2f1e3
daniBerAs <119543171+daniBerAs@users.noreply.github.com>
2025-06-18 11:32:44 +0200

    Update __init__.py

--- MMI_solver/__init__.py
+++ MMI_solver/__init__.py
@@ -4,4 +4,4 @@ from .funciones import create_ridge2
 from .funciones import create_taper
 from .funciones import var_eps_eff
 from .funciones import approximate_material
-from .funciones import create_2d_sim
+from .funciones import create_2D_sim
